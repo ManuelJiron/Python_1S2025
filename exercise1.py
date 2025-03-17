2ef450649a565c24157211f115862b987429e9c0
ManuelJiron <majiron@uamv.edu.ni>
2025-03-17 10:33:21 -0600

    Sesion 1 17 de marzo

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+#Comentario de una linea
+
+"""
+imprimir los datos personales del estudiante
+"""
+
+
+print("Datos del Estudiante")
+print("=" * 20)
+print("Nombre: ", end= "")
+print("Jose Alejandro")
